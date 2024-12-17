afc2f3dff3bdd504668f4be2fdea9c73010950a2
androidDeveloperOne <android.developer@knestaluform.in>
2024-12-17 16:49:03 +0530

    chnages

--- src/constants/index.js
+++
@@ -1,5 +0,0 @@
-export const accessKeyId = process.env.REACT_APP_AWS_ACCESSKEYID;
-export const secretAccessKey = process.env.REACT_APP_AWS_SECRETACCESSKEY;
-export const bucketName = process.env.REACT_APP_AWS_BUCKETNAME;
-export const region = process.env.REACT_APP_AWS_REGION;
-export const encryptionkey = process.env.REACT_APP_ENCRYPTKEY;
